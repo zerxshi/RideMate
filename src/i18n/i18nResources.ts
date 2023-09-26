8a54b726685db8a456eabc3067fa2930a82c5ae2
Radmir Musabaev <musabaevradmir@gmail.com>
2023-09-26 14:10:37 +0400

    i18n config configured

---
+++ src/i18n/i18nResources.ts
@@ -0,0 +1,14 @@
+import enCommon from "./en/common.json"
+import ruCommon from "./ru/common.json"
+
+const resources = {
+    en: {
+        //Тут модули файлов с переводами
+        common: enCommon
+    },
+    ru: {
+        common: ruCommon
+    }
+}
+
+export default resources
