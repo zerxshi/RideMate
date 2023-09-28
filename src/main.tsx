00999c170d378b73816d0df909bf3258b81afd23
Arsen Bazarbayev <zerxshii@gmail.com>
2023-09-28 16:07:49 +0600

    Added font-awesome icons to the project

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import "./index.css"
 import "./i18n/config"
 import { Provider } from "react-redux"
 import { store } from "./store"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faAngleDown, fas } from "@fortawesome/free-solid-svg-icons"
+
+library.add(fas, faAngleDown)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
--- src/modules/AppHeader/components/Header.tsx
+++ src/modules/AppHeader/components/Header.tsx
@@ -5,31 +5,28 @@ import {
     MAIN_PAGE_ROUTE,
     PROFILE_ROUTE,
 } from "@/modules/AppRouter/utils/consts"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Header: FC = () => {
     return (
         <header className="flex justify-center bg-my-gray">
             <nav className="flex items-center justify-between w-1800">
                 <Link to={MAIN_PAGE_ROUTE}>
-                    <a>
-                        <img
-                            src={logo}
-                            alt="rideMate logo"
-                            className="w-40 p-3"
-                        />
-                    </a>
+                    <img src={logo} alt="rideMate logo" className="w-40 p-3" />
                 </Link>
                 <button>
                     <h1 className="text-5xl font-bold text-my-white">
                         Ride Mate
                     </h1>
+                    <FontAwesomeIcon
+                        icon="fa-solid fa-angle-down"
+                        className="text-2xl text-my-white"
+                    />
                 </button>
                 <Link to={PROFILE_ROUTE}>
-                    <a>
-                        <p className="text-4xl font-bold text-my-white">
-                            Login/Register
-                        </p>
-                    </a>
+                    <p className="text-4xl font-bold text-my-white">
+                        Login/Register
+                    </p>
                 </Link>
             </nav>
         </header>
