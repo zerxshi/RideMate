6031f24491278345ca117896fe318c4386e5c517
Radmir Musabaev <musabaevradmir@gmail.com>
2023-09-30 22:08:29 +0400

    Создал и подключил userSlice

---
+++ src/store/slice/userSlice.ts
@@ -0,0 +1,51 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode"
+
+interface IUser {
+    email: string | null;
+    id: number | null;
+    role: string | null;
+    isAuth: boolean;
+    isActivated: boolean
+}
+
+interface IDecodeToken {
+    email: string;
+    id: number;
+    role: string,
+    isActivated: boolean
+}
+
+const initialState: IUser = {
+    email: null,
+    id: null,
+    role: null,
+    isAuth: false,
+    isActivated: false
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<string>) => {
+            const jwttoken: IDecodeToken = jwtDecode(action.payload)
+            state.email = jwttoken.email
+            state.id = jwttoken.id
+            state.role = jwttoken.role
+            state.isActivated = jwttoken.isActivated
+            state.isAuth = true
+        },
+        deleteUser: (state) => {
+            state.email =  null
+            state.id = null
+            state.role = null
+            state.isAuth = false
+            state.isActivated = false
+            localStorage.setItem('accessToken', '')
+        }
+    }
+})
+
+export const {setUser, deleteUser} = userSlice.actions
+export default userSlice.reducer
