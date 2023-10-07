50661f80518364f8c1e33c51deb87bce1cc2986e
Arsen Bazarbayev <zerxshii@gmail.com>
2023-10-07 20:34:50 +0600

    Вынес блок инпута в отдельную компоненту

--- src/modules/LoginForm/components/LoginFormInputs.tsx
+++ src/modules/LoginForm/components/LoginFormInputs.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useState } from "react"
 import { useTranslation } from "react-i18next"
+import LoginInputBlock from "./LoginInputBlock"
+import RegisterInputBlock from "./RegisterInputBlock"
 
 interface FormInputProps {
     emailValue: string
@@ -40,113 +42,41 @@ const LoginFormInputs: FC<FormInputProps> = ({
                 {t("phrases.registration")}
             </b>
 
-            <div
-                className={`flex flex-col ${
-                    isLogin ? "hidden" : "animate-append"
-                }`}
-            >
-                <label
-                    className={`text-xl font-bold text-my-dark -translate-y-3 translate-x-4  bg-my-blue ${
-                        nameValue
-                            ? "absolute w-max transition-all duration-200 origin-bottom-right"
-                            : "sr-only"
-                    }`}
-                    htmlFor="name"
-                >
-                    Name
-                </label>
-                <input
-                    className="p-2 text-2xl font-bold bg-transparent border-4 h-14 rounded-2xl border-my-dark text-my-dark placeholder:text-my-dark"
-                    value={nameValue}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setNameValue(e.target.value)
-                    }
-                    id="name"
-                    type="text"
-                    placeholder={t("inputs.name")}
-                />
-            </div>
+            <RegisterInputBlock
+                isLogin={isLogin}
+                inputValue={nameValue}
+                setInputValue={setNameValue}
+                inputId={"name"}
+                inputType={"text"}
+                labelTitle={"Name"}
+            />
 
-            <div
-                className={`flex flex-col ${
-                    isLogin ? "" : "animate-slideDown"
-                }`}
-            >
-                <label
-                    className={`text-xl font-bold text-my-dark -translate-y-3 translate-x-4  bg-my-blue ${
-                        emailValue
-                            ? "absolute w-max transition-all duration-200 origin-bottom-right"
-                            : "sr-only"
-                    }`}
-                    htmlFor="email"
-                >
-                    Email
-                </label>
-                <input
-                    className="p-2 px-3 text-2xl font-bold bg-transparent border-4 h-14 rounded-2xl border-my-dark text-my-dark placeholder:text-my-dark"
-                    value={emailValue}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setEmailValue(e.target.value)
-                    }
-                    id="email"
-                    type="email"
-                    placeholder={t("inputs.email")}
-                />
-            </div>
+            <LoginInputBlock
+                isLogin={isLogin}
+                inputValue={emailValue}
+                setInputValue={setEmailValue}
+                inputId={"email"}
+                inputType={"email"}
+                labelTitle={"Email"}
+            />
 
-            <div
-                className={`flex flex-col ${
-                    isLogin ? "" : "animate-slideDown"
-                }`}
-            >
-                <label
-                    className={`text-xl font-bold text-my-dark -translate-y-3 translate-x-4  bg-my-blue ${
-                        passwordValue
-                            ? "absolute w-max transition-all duration-200 origin-bottom-right"
-                            : "sr-only"
-                    }`}
-                    htmlFor="password"
-                >
-                    Password
-                </label>
-                <input
-                    className="p-2 text-2xl font-bold bg-transparent border-4 h-14 rounded-2xl border-my-dark text-my-dark placeholder:text-my-dark"
-                    value={passwordValue}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setPasswordValue(e.target.value)
-                    }
-                    id="password"
-                    type="password"
-                    placeholder={t("inputs.password")}
-                />
-            </div>
+            <LoginInputBlock
+                isLogin={isLogin}
+                inputValue={passwordValue}
+                setInputValue={setPasswordValue}
+                inputId={"password"}
+                inputType={"password"}
+                labelTitle={"Password"}
+            />
 
-            <div
-                className={`flex flex-col ${
-                    isLogin ? "hidden" : "animate-append"
-                }`}
-            >
-                <label
-                    className={`text-xl font-bold transition text-my-dark -translate-y-3 translate-x-4  bg-my-blue ${
-                        confirmPasswordValue
-                            ? "absolute w-max transition-all duration-200 origin-bottom-right"
-                            : "sr-only"
-                    }`}
-                    htmlFor="confirmPassword"
-                >
-                    Confirm password
-                </label>
-                <input
-                    className="p-2 text-2xl font-bold bg-transparent border-4 h-14 rounded-2xl border-my-dark text-my-dark placeholder:text-my-dark"
-                    value={confirmPasswordValue}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setConfirmPasswordValue(e.target.value)
-                    }
-                    id="confirmPassword"
-                    type="password"
-                    placeholder={t("inputs.confirmPassword")}
-                />
-            </div>
+            <RegisterInputBlock
+                isLogin={isLogin}
+                inputValue={confirmPasswordValue}
+                setInputValue={setConfirmPasswordValue}
+                inputId={"confirmPassword"}
+                inputType={"password"}
+                labelTitle={"Confirm password"}
+            />
 
             {isLogin && (
                 <b className="self-end font-black text-my-dark">
