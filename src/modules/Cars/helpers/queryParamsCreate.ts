c9a22c581bc868a3345ebc8503add5de9a3202d6
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-30 22:55:59 +0400

    написал функцию для генерации query параметров

---
+++ src/modules/Cars/helpers/queryParamsCreate.ts
@@ -0,0 +1,44 @@
+export const queryParamsCreate = (stringParams: string) => {
+    const queryParams = new URLSearchParams()
+    const params = new URLSearchParams(stringParams)
+    const classId = params.get("cars")
+    if (classId) {
+        if (getClassId(classId) !== -1) {
+            queryParams.set("classId", getClassId(classId).toString());
+        }
+    }
+    if (params.get("brandId")) {
+        queryParams.set("brandId", params.get("brandId")!)
+    }
+    if (params.get("minPrice")) {
+        queryParams.set("minPrice", params.get("minPrice")!)
+    }
+    if (params.get("maxPrice")) {
+        queryParams.set("maxPrice", params.get("maxPrice")!)
+    }
+    if (params.get("limit")) {
+        queryParams.set("limit", params.get("limit")!)
+    }
+    if (params.get("page")) {
+        queryParams.set("page", params.get("page")!)
+    }
+
+    return queryParams
+}
+
+const getClassId = (stringClass: string): number => {
+    if (stringClass === "sClass") {
+        return 1
+    }
+    if (stringClass === "bClass") {
+        return 2
+    }
+    if (stringClass === "fClass") {
+        return 3
+    }
+    if (stringClass === "crossovers") {
+        return 4
+    }
+
+    return -1
+}
