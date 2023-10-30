ffaf8606f8f720752b8b11378b62f6a2e8da83b0
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-30 22:54:57 +0400

    настроил АПИ для классов автомобилей

---
+++ src/modules/Cars/API/classAPI.ts
@@ -0,0 +1,17 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {BASE_API_URL} from "@/utils/consts";
+import {IResponseClass} from "@/modules/Cars/types";
+
+export const classAPI = createApi({
+    reducerPath: "classAPI",
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASE_API_URL + "/class"
+    }),
+    endpoints: (build) => ({
+        getAllClasses: build.query<IResponseClass, void>({
+            query: () => ({
+                url: "/",
+            })
+        })
+    })
+})
