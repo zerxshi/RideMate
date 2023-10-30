fc11443aee935a74728b64a5340b3881f1d7f5da
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-30 22:53:03 +0400

    подключил carsAPI, brandAPI, classAPI к стору

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,27 @@
 import { authAPI } from "@/modules/LoginForm"
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import userSlice from "./slice/userSlice"
+import { carsAPI } from "@/modules/Cars";
+import { brandAPI } from "@/modules/Cars";
+import { classAPI } from "@/modules/Cars";
 
 const rootReducer = combineReducers({
     userReducer: userSlice,
     [authAPI.reducerPath]: authAPI.reducer,
+    [carsAPI.reducerPath]: carsAPI.reducer,
+    [brandAPI.reducerPath]: brandAPI.reducer,
+    [classAPI.reducerPath]: classAPI.reducer
 })
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authAPI.middleware),
+        getDefaultMiddleware().concat(
+            authAPI.middleware,
+            carsAPI.middleware,
+            brandAPI.middleware,
+            classAPI.middleware
+        ),
 })
 
 export type RootState = ReturnType<typeof store.getState>
