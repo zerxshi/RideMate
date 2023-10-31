f199559dc76c64f2c06ce78e25a52ea40aa90f23
Arsen Bazarbayev <zerxshii@gmail.com>
2023-10-31 15:52:07 +0600

    Поменял моковый isAuth на настоящий

--- src/modules/AppRouter/Components/AppRouter.tsx
+++ src/modules/AppRouter/Components/AppRouter.tsx
@@ -1,29 +1,30 @@
+import { useAppSelector } from "@/hooks/useTypedStore"
 import React, { FunctionComponent } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { privatePages, publicPages } from "../routes"
 
 const AppRouter: FunctionComponent = () => {
-    const isAuth = true
+    const { isAuth } = useAppSelector((state) => state.userReducer)
 
     return (
         <Routes>
-            {isAuth && privatePages.map(route =>
+            {isAuth &&
+                privatePages.map((route) => (
                     <Route
                         path={route.path}
-                        element={<route.component/>}
+                        element={<route.component />}
                         key={route.path}
                     />
-                )}
+                ))}
 
-            {publicPages.map(route =>
-                    <Route
-                        path={route.path}
-                        element={<route.component/>}
-                        key={route.path}
-                    />
-                )
-            }
-            <Route path={'*'} element={<Navigate to={'/'} replace={true}/> }/>
+            {publicPages.map((route) => (
+                <Route
+                    path={route.path}
+                    element={<route.component />}
+                    key={route.path}
+                />
+            ))}
+            <Route path={"*"} element={<Navigate to={"/"} replace={true} />} />
         </Routes>
     )
 }
