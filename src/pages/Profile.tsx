7b6c56001d310f5aab45426b427b67cd3a8eeb29
Arsen Bazarbayev <zerxshii@gmail.com>
2023-10-31 15:53:21 +0600

    Создал нужные файлы для страницы личного кабинета

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,12 @@
+import { UserProfile } from "@/modules/UserProfile"
 import React, { FunctionComponent } from "react"
 
 const Profile: FunctionComponent = () => {
-    return <main>LK</main>
+    return (
+        <main className="flex justify-center w-screen h-screen mt-10">
+            <UserProfile />
+        </main>
+    )
 }
 
 export default Profile
