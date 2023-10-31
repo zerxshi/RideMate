a2fef51ce22b7fd34e7f1a74936b9ed72d423244
Arsen Bazarbayev <zerxshii@gmail.com>
2023-10-31 18:52:17 +0600

    Создано API для отправки запроса на смену данных

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,21 @@
 import { authAPI } from "@/modules/LoginForm"
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import userSlice from "./slice/userSlice"
+import { changeDataAPI } from "@/modules/UserProfile/API/ChangeDataAPI"
 
 const rootReducer = combineReducers({
     userReducer: userSlice,
     [authAPI.reducerPath]: authAPI.reducer,
+    [changeDataAPI.reducerPath]: changeDataAPI.reducer,
 })
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authAPI.middleware),
+        getDefaultMiddleware().concat(
+            authAPI.middleware,
+            changeDataAPI.middleware,
+        ),
 })
 
 export type RootState = ReturnType<typeof store.getState>
