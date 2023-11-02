5f0ba503a91c830be42508e645b0727c4f68cc50
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-02 23:41:35 +0600

    Убрал консоль лог авто логина

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,24 @@ import React, { useEffect } from "react"
 import { BrowserRouter } from "react-router-dom"
 import { Header } from "./modules/AppHeader"
 import AppRouter from "./modules/AppRouter"
-import {autoLoginAPI} from "@/API/autoLoginAPI";
-import {setUser} from "@/store/slice/userSlice";
-import {useAppDispatch} from "@/hooks/useTypedStore";
+import { autoLoginAPI } from "@/API/autoLoginAPI"
+import { setUser } from "@/store/slice/userSlice"
+import { useAppDispatch } from "@/hooks/useTypedStore"
 
 const App = () => {
     const [mutate] = autoLoginAPI.useAutoLoginMutation()
     const dispatch = useAppDispatch()
 
-    useEffect( () => {
+    useEffect(() => {
         const loginUser = async () => {
             const result = await mutate()
             if ("data" in result) {
                 dispatch(setUser(result.data))
-                console.log("login")
             }
         }
 
         loginUser()
-    }, []);
+    }, [])
 
     return (
         <BrowserRouter>
