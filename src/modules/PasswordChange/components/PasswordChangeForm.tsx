6194f49f864a7d58ccefd3d98890e96ec7faa240
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-06 15:50:22 +0600

    Создан компонент формы для смены пароля и прилегающие к нему компоненты

---
+++ src/modules/PasswordChange/components/PasswordChangeForm.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PasswordChangeForm = () => {
+    return (
+        <section>
+            <form></form>
+        </section>
+    )
+}
+
+export { PasswordChangeForm }
