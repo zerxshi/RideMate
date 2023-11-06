3bf7c5e8a77e93f114334f2143d0750422362919
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-06 15:49:26 +0600

    Создана страницы смены пароля

---
+++ src/pages/PasswordChange.tsx
@@ -0,0 +1,12 @@
+import { PasswordChangeForm } from "@/modules/PasswordChange"
+import React, { FC } from "react"
+
+const PasswordChange: FC = () => {
+    return (
+        <main>
+            <PasswordChangeForm />
+        </main>
+    )
+}
+
+export default PasswordChange
