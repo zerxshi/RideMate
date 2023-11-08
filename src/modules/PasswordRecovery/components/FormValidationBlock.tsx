714fad60d3c2bbe8c456eea6e4bfe8d0ef9f7c87
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-08 21:34:37 +0600

    Добавлен перевод ошибки для токена восстановления пароля, добавлены ошибки в блок по валидации

--- src/modules/PasswordRecovery/components/FormValidationBlock.tsx
+++ src/modules/PasswordRecovery/components/FormValidationBlock.tsx
@@ -1,10 +1,28 @@
+import { IError } from "@/types"
 import React, { FC } from "react"
+import { useTranslation } from "react-i18next"
 
 interface ValidationBlockProps {
     validationError: string
+    isRecoveryError: boolean
+    isRequestError: boolean
+    isTokenError: boolean
+    recoveryError: IError
+    requestError: IError
+    tokenError: IError
 }
 
-const FormValidationBlock: FC<ValidationBlockProps> = ({ validationError }) => {
+const FormValidationBlock: FC<ValidationBlockProps> = ({
+    validationError,
+    recoveryError,
+    requestError,
+    tokenError,
+    isRecoveryError,
+    isRequestError,
+    isTokenError,
+}) => {
+    const { t } = useTranslation("common")
+
     return (
         <div>
             <strong
@@ -13,6 +31,30 @@ const FormValidationBlock: FC<ValidationBlockProps> = ({ validationError }) => {
                 }`}
             >
                 {validationError}
+            </strong>
+
+            <strong
+                className={`ml-3 font-bold text-my-dark text-lg ${
+                    isRequestError ? "animate-append" : "hidden"
+                }`}
+            >
+                {requestError && t(`errors.${requestError.data.message}`)}
+            </strong>
+
+            <strong
+                className={`ml-3 font-bold text-my-dark text-lg ${
+                    isTokenError ? "animate-append" : "hidden"
+                }`}
+            >
+                {tokenError && t(`errors.${tokenError.data.message}`)}
+            </strong>
+
+            <strong
+                className={`ml-3 font-bold text-my-dark text-lg ${
+                    isRecoveryError ? "animate-append" : "hidden"
+                }`}
+            >
+                {recoveryError && t(`errors.${recoveryError.data.message}`)}
             </strong>
         </div>
     )
