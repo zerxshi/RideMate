faea2419dc5774b9a9ec36872035204291397b74
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-14 16:40:24 +0600

    Создал функцию для нахождения авто по его id и функцию для создания строки из массива дат

--- src/modules/UserProfile/components/RentalHistory.tsx
+++ src/modules/UserProfile/components/RentalHistory.tsx
@@ -1,15 +1,38 @@
 import React, { FC } from "react"
-import { rentalHistoryAPI } from "../API/rentalHistoryAPI"
-import RentalCarCard from "./RentalCarCard"
+import RentalCarCard from "@/modules/UserProfile/components/RentalCarCard"
+import { ICar, IRentalCar } from "@/modules/UserProfile/types"
 
-const RentalHistory: FC = () => {
-    const { data: rentalHistory } = rentalHistoryAPI.useGetRentalHistoryQuery()
+interface RentalHistoryProps {
+    cars: ICar[]
+    rentalCars: IRentalCar[]
+}
+
+const RentalHistory: FC<RentalHistoryProps> = ({ cars, rentalCars }) => {
+    const findCarById = (carId: number): ICar | undefined => {
+        return cars.find((car) => car.id === carId)
+    }
+
+    const rentalDates = (dates: string[]): string => {
+        if (dates.length <= 2) {
+            return dates.join(", ")
+        }
+        if (dates.length > 2) {
+            return `${dates[0]} - ${dates[dates.length - 1]}`
+        }
+
+        return ""
+    }
 
     return (
         <section className="flex flex-col gap-4">
-            {rentalHistory &&
-                rentalHistory.rows &&
-                rentalHistory.rows.map(() => <RentalCarCard />)}
+            {rentalCars.map((rentalCar) => (
+                <RentalCarCard
+                    car={findCarById(rentalCar.carId)}
+                    totalPrice={rentalCar.totalPrice}
+                    rentalDates={rentalDates(rentalCar.occupied_dates)}
+                    key={rentalCar.id}
+                />
+            ))}
         </section>
     )
 }
