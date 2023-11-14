667aa4736b3fc85a3d6befdd6d326baa199d2ab9
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-14 16:40:46 +0600

    Поменял статичные данные на те, что приходят с сервера

--- src/modules/UserProfile/components/RentalCarCard.tsx
+++ src/modules/UserProfile/components/RentalCarCard.tsx
@@ -1,24 +1,32 @@
-import React from "react"
-import polo from "@/assets/images/cars/b-class-polo.jpg"
+import React, { FC } from "react"
+import { ICar } from "@/modules/UserProfile/types"
+import { BASE_SERVER_URL } from "@/utils/consts"
+interface RentalCarCardProps {
+    car: ICar | undefined
+    rentalDates: string
+    totalPrice: number
+}
 
-interface RentalCarCardProps {}
-
-const RentalCarCard = () => {
+const RentalCarCard: FC<RentalCarCardProps> = ({
+    car,
+    rentalDates,
+    totalPrice,
+}) => {
     return (
-        <article className="flex gap-5 p-4 shadow-xl bg-zinc-700 rounded-xl w-605">
-            <img src={polo} className="w-64" />
+        <article className="flex gap-5 p-4 shadow-xl bg-zinc-700 rounded-xl w-[630px]">
+            <img src={BASE_SERVER_URL + car!.img} className="w-64 rounded-lg" />
 
             <div>
-                <b className="text-xl">VOLKSWAGEN POLO</b>
-                <dl className="flex flex-wrap gap-1 text-lg font-bold ">
+                <b className="text-xl">{car?.model}</b>
+                <dl className="flex flex-wrap gap-1 text-lg font-bold">
                     <dt>Rental dates:</dt>
-                    <dd>10.11.23, 29.11.23</dd>
+                    <dd>{rentalDates}</dd>
                 </dl>
-                <dl className="flex gap-1 text-lg font-bold ">
+                <dl className="flex gap-1 text-lg font-bold">
                     <dt>Total price:</dt>
-                    <dd>40000</dd>
+                    <dd>{totalPrice}</dd>
                 </dl>
-                <dl className="flex gap-1 text-lg font-bold ">
+                <dl className="flex gap-1 text-lg font-bold">
                     <dt>Status:</dt>
                     <dd>Done</dd>
                 </dl>
