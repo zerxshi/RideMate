6031fd1e0c2b765790f625754c3d9c3486d4116a
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-22 23:14:57 +0600

    Функция по удалению класса имплементирована в список классов

--- src/modules/AdminPanel/components/ClassCard.tsx
+++ src/modules/AdminPanel/components/ClassCard.tsx
@@ -3,9 +3,10 @@ import React, { FC } from "react"
 interface ClassCardProps {
     classId: number
     className: string
+    removeClass: (classId: number) => Promise<void>
 }
 
-const ClassCard: FC<ClassCardProps> = ({ classId, className }) => {
+const ClassCard: FC<ClassCardProps> = ({ classId, className, removeClass }) => {
     return (
         <article className="flex flex-col  p-4 shadow-xl bg-zinc-700 rounded-xl w-[630px]">
             <dl className="flex gap-1 text-lg font-bold">
@@ -17,6 +18,7 @@ const ClassCard: FC<ClassCardProps> = ({ classId, className }) => {
                 <dd>{className}</dd>
             </dl>
             <button
+                onClick={() => removeClass(classId)}
                 className="self-end h-8 px-2 text-lg font-bold rounded-lg w-max bg-my-blue text-my-dark"
                 type="button"
             >
--- src/modules/AdminPanel/components/ClassesList.tsx
+++ src/modules/AdminPanel/components/ClassesList.tsx
@@ -4,9 +4,10 @@ import ClassCard from "@/modules/AdminPanel/components/ClassCard"
 
 interface ClassesListProps {
     classes: IClass[]
+    removeClass: (classId: number) => Promise<void>
 }
 
-const ClassesList: FC<ClassesListProps> = ({ classes }) => {
+const ClassesList: FC<ClassesListProps> = ({ classes, removeClass }) => {
     return (
         <section className="flex flex-col gap-4">
             {classes.map((carClass) => (
@@ -14,6 +15,7 @@ const ClassesList: FC<ClassesListProps> = ({ classes }) => {
                     key={carClass.id}
                     classId={carClass.id}
                     className={carClass.name}
+                    removeClass={removeClass}
                 />
             ))}
         </section>
