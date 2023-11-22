5b30b2204fa8ccbb7cb05a135b8aaa6c94185266
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-22 16:06:58 +0600

    Создал индивидуальную  карточку машины для списка авто в админ панели

---
+++ src/modules/AdminPanel/components/AdminCarCard.tsx
@@ -0,0 +1,53 @@
+import { BASE_SERVER_URL } from "@/utils/consts"
+import React, { FC } from "react"
+
+interface AdminCarCardProps {
+    carId: number
+    img: string
+    model: string
+    brand: string | undefined
+    carClass: string | undefined
+}
+
+const AdminCarCard: FC<AdminCarCardProps> = ({
+    carId,
+    img,
+    model,
+    brand,
+    carClass,
+}) => {
+    return (
+        <article className="flex gap-5 p-4 shadow-xl bg-zinc-700 rounded-xl w-[630px]">
+            <img src={BASE_SERVER_URL + img} className="w-64 rounded-lg" />
+
+            <div className="flex flex-col gap-14 w-80">
+                <div>
+                    <b className="text-xl">
+                        {brand} {model}
+                    </b>
+                    <dl className="flex flex-wrap gap-1 text-lg font-bold">
+                        <dt>Car ID:</dt>
+                        <dd>{carId}</dd>
+                    </dl>
+                    <dl className="flex gap-1 text-lg font-bold">
+                        <dt>Segment:</dt>
+                        <dd>{carClass}</dd>
+                    </dl>
+                    <dl className="flex gap-1 text-lg font-bold">
+                        <dt>Brand:</dt>
+                        <dd>{brand}</dd>
+                    </dl>
+                </div>
+
+                <button
+                    className="self-end h-8 px-2 text-lg font-bold rounded-lg w-max bg-my-blue text-my-dark"
+                    type="button"
+                >
+                    Remove the car
+                </button>
+            </div>
+        </article>
+    )
+}
+
+export default AdminCarCard
