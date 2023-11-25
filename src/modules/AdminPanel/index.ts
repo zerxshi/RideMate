d4ac58c0ac4b98391183a92bb61a31c5d04c8ce3
Arsen Bazarbayev <zerxshii@gmail.com>
2023-11-25 15:59:02 +0600

    Удалил АПИ по удалению классов и брендов, т.к. оно более не нужно

--- src/modules/AdminPanel/index.ts
+++ src/modules/AdminPanel/index.ts
@@ -1,4 +1,2 @@
 export { AdminPanel } from "@/modules/AdminPanel/components/AdminPanel"
-export { brandDeletionAPI } from "@/modules/AdminPanel/API/brandDeletionAPI"
-export { classDeletionAPI } from "@/modules/AdminPanel/API/classDeletionAPI"
 export { adminCarsAPI } from "@/modules/AdminPanel/API/carsAPI"
