a1a37b3a6622472799a7c2f4038d3d08fc796a1f
Radmir Musabaev <musabaevradmir@gmail.com>
2023-11-30 16:27:56 +0400

    реализовал сортировку по модели и по цене

--- src/modules/Cars/components/CarsSort.tsx
+++ src/modules/Cars/components/CarsSort.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
 import {SetURLSearchParams} from "react-router-dom";
 
@@ -7,15 +7,70 @@ interface Props {
     setSearchParams: SetURLSearchParams;
 }
 
-const CarsSort: FC<Props> = () => {
+const CarsSort: FC<Props> = ({ setSearchParams, searchParams}) => {
     const { t } = useTranslation("carsPage")
+    const [selectedSort, setSelectedSort] = useState<string| undefined>(undefined)
+    const [sortMethod, setSortMethod] = useState<string | undefined>(undefined)
+
+    useEffect(() => {
+        setSearchParams(params => {
+            if (selectedSort) {
+                params.set("sort", selectedSort)
+            }
+            return params
+        })
+    }, [selectedSort]);
+
+    useEffect(() => {
+        setSearchParams(params => {
+            if (sortMethod) {
+                params.set("sortBy", sortMethod)
+            }
+            return params
+        })
+    }, [sortMethod]);
 
     return (
         <div>
             {t("sortAndFilters.sortBy")}
             <ul>
                 <li>
-
+                    <select
+                        value={selectedSort}
+                        onChange={event => setSelectedSort(event.target.value)}
+                        defaultValue={undefined}
+                    >
+                        <option
+                            value={"model"}
+                        >
+                            Model
+                        </option>
+                        <option
+                            value={"price"}
+                        >
+                            Price
+                        </option>
+                    </select>
+                </li>
+                <li>
+                    <select
+                        value={sortMethod}
+                        onChange={event => setSortMethod(event.target.value)}
+                        defaultValue={undefined}
+                    >
+                        <option
+                            value={"asc"}
+                            onChange={() => setSortMethod("asc")}
+                        >
+                            Asc
+                        </option>
+                        <option
+                            value={"desc"}
+                            onChange={() => setSortMethod("desc")}
+                        >
+                            Desc
+                        </option>
+                    </select>
                 </li>
             </ul>
         </div>
